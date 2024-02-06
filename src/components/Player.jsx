90dabc13510080366f73f080a14e4127d8da87e8
Rohit <rohityadav787898@gmail.com>
2024-02-06 18:19:49 +0545

    wrapped refs section

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,19 @@
+import { useRef, useState } from "react";
+
 export default function Player() {
+  const playerName = useRef(); // To prevent unmounting and recreating the player on every render.
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+
+  function hanldeClick() {
+    setEnteredPlayerName(playerName.current.value);
+    playerName.current.value = "";
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayerName ?? " unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text" />
+        <button onClick={hanldeClick}>Set Name</button>
       </p>
     </section>
   );
